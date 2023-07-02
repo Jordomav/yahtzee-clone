203a50f790e816472c71d534e6e870335cdd316e
Jordan Mavrogeorge <jordomav@gmail.com>
2023-07-02 14:33:01 -0400

    init

---
+++ YhatzeeClone/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  YhatzeeClone
+//
+//  Created by Jordan Mavrogeorge on 7/2/23.
+//
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ YhatzeeClone/YhatzeeCloneApp.swift
@@ -0,0 +1,18 @@
+//
+//  YhatzeeCloneApp.swift
+//  YhatzeeClone
+//
+//  Created by Jordan Mavrogeorge on 7/2/23.
+//
+//
+
+import SwiftUI
+
+@main
+struct YhatzeeCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
